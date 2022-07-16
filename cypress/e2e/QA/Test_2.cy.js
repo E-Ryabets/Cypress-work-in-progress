ccb3088bbec93a481d374d8b22b284c3af423ef3
Egor <egorbucks@gmail.com>
2022-07-16 23:37:51 +0300

    Saturday

--- cypress/e2e/QA/Test_2.cy.js
+++ cypress/e2e/QA/Test_2.cy.js
@@ -1,7 +1,8 @@
-describe('Guitar bank test 1', () => {
+describe('Guitar bank test 2', () => {
     it('Найти контент', () => {
       cy.visit('https://guitarbank.ru/')
   
       cy.contains('Все что указано на сайте — реальные инструменты')
+      cy.get('.todo-list li').should('have.length', 2)
     })
   })
